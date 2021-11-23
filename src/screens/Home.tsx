53c2d0fa085b1a4294a3b3620db62f4dfc3481af
Sebastian Cordero <scordero414@gmail.com>
2021-11-23 01:09:25 -0500

    Get Products from firebase

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -11,29 +11,59 @@ import {
   Button,
   Image,
 } from "native-base";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { COLORS } from "../resources/Constants";
 import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 import { ScrollCategories } from "../components/ScrollCategories";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { store } from "../firebaseUtil/firebaseConfig";
+import { Product } from "../models/Product";
 
 export const Home = () => {
+  const [products, setProducts] = useState<Product[] | null>(null);
 
-    const createProduct = () => {
-        store.collection("products").add({
-            name: "Tokyo",
-            country: "Japan"
-        })
-        .then((docRef: any) => {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch((error: string) => {
-            console.error("Error adding document: ", error);
-        });
-        
-    }
+  useEffect(() => {
+    getProducts();
+  }, []);
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
+
+  const getProducts = async () => {
+    const { docs } = await store.collection("products").get();
+    const arr = docs.map((item: any) => ({ id: item.id, ...item.data() }));
+    setProducts(arr);
+  };
+
+  const createProduct = () => {
+    const product: Product = {
+      name: "Camibuzo azul oscuro UAM",
+      category: ["ropa", "camibuzo"],
+      size: [
+        { size: "S", stock: 22 },
+        { size: "M", stock: 3 },
+        { size: "L", stock: 7 },
+      ],
+      description:
+        "qweqweqweqweqeqwewqqqqqqqqweqweqweqweqweqweqweqweqweqweqweqweqweqqweqwe",
+      color: "Azul oscuro",
+      image:
+        "https://static.dafiti.com.co/p/nautica-3232-4848601-1-product.jpg",
+      price: 1750000,
+      createdAt: new Date(),
+    };
+
+    store
+      .collection("products")
+      .add(product)
+      .then((docRef: any) => {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch((error: string) => {
+        console.error("Error adding document: ", error);
+      });
+  };
 
   return (
     <SafeAreaView style={{ backgroundColor: "white" }}>
@@ -62,7 +92,7 @@ export const Home = () => {
                 bgColor={COLORS.BLUE}
                 variant="solid"
                 borderRadius={100}
-                onPress={()=>createProduct()}
+                // onPress={()=>createProduct()}
                 _icon={{
                   as: MaterialIcons,
                   name: "search",
